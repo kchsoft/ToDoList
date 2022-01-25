4e6d62406ddefe6c6737acc56ebd5b727c298334
changhyeon3 <53136613+changhyeon3@users.noreply.github.com>
2022-01-25 14:07:28 +0900

    ToDoList 작성 부분 3개 구현 중
    +93 부분 부터 수정해야 함.
    
    & toDoForm , toDoInput , toDoList를 모두 배열로 변경하였으며,
    eventListener의 매개변수 event를 이용하여 Form을 찾아내는 함수를 추가하였다. +24 , +30 todo.js
    
    & toDos 또한 배열을 3개로 나누었으며, 이를 찾는 함수 또한 추가하였다. +14 todo.js

--- JavaScript/todo.js
+++ JavaScript/todo.js
@@ -1,25 +1,58 @@
-const toDoForm = document.querySelector(".todo-form");
-const toDoInput = toDoForm.querySelector("input");
-// const toDoInput = document.querySelector("#todo-form input"); 2와 같다.
-const toDoList = document.querySelector(".todo-list");
+const toDoForm = document.querySelectorAll(".todo-form"); // Array
+const toDoInput = document.querySelectorAll("main input");
+const toDoList = document.querySelectorAll(".todo-list");
 
-let toDos = [];
-const TODOS_KEY="todos";
+
+let toDos1 = [];
+let toDos2 = [];
+let toDos3 = [];
+const toDoFormSize= 3;
+
+let toDosnum;
+const TODOS_KEY=["form_1","form_2","form_3"];
+
+function findToDos(event){
+    if(event.target === toDoForm[0]) return toDos1;
+    if(event.target === toDoForm[1]) return toDos2;
+    if(event.target === toDoForm[2]) return toDos3;
+
+    if(event === 0) return toDos1;
+    if(event === 1) return toDos2;
+    if(event === 2) return toDos3;
+}
+
+function findToDoForm(event){
+    if(event.target === toDoForm[0]) return event.target;
+    if(event.target === toDoForm[1]) return event.target;
+    if(event.target === toDoForm[2]) return event.target;
+}
+
+function findToDoFormNumber(event){  
+    if(event.target === toDoForm[0]) return 0;
+    if(event.target === toDoForm[1]) return 1;
+    if(event.target === toDoForm[2]) return 2;
+
+}
 
 function handleToDoSubmit(event){
     event.preventDefault();
-    const newTodo = toDoInput.value;
-    toDoInput.value="";
+    const toDoInputTarget = findToDoForm(event);
+    const newTodo = toDoInputTarget.value;
+    toDoInputTarget.value="";
     const newTodoObj = {
         text : newTodo,
         id : Date.now()
     };
-    toDos.push(newTodoObj);
-    paintToDo(newTodoObj);
-    saveToDos();
+
+    const toDo = findToDos(event);
+    toDo.push(newTodoObj);
+    paintToDo(newTodoObj,event);
+    saveToDos(event);
 }
 
-function paintToDo(newTodo){
+function paintToDo(newTodo,event){
+    // if(newTodo === null) return;
+
     const p = document.createElement("p");
     p.id = newTodo.id;
     
@@ -32,11 +65,11 @@ function paintToDo(newTodo){
     
     p.appendChild(span);
     p.appendChild(button);
-    toDoList.appendChild(p);
+    toDoList[findToDoFormNumber(event)].appendChild(p);
 }
 
-function saveToDos(){
-    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
+function saveToDos(event){
+    localStorage.setItem(TODOS_KEY[findToDoFormNumber(event)],JSON.stringify(findToDos(event)));
 }
 
 function deleteToDo(event){
@@ -48,15 +81,25 @@ function deleteToDo(event){
     // toDo로 요소하나를 받고, => 오른쪽으로 연산을 하며 true면 새로운 array에 포함,
     // false면 새로운 array에서 제외한다.
     // 결과적으로 true가 반환되는 toDo를 새로운 array에 포함한다. -> 새로운 array 만듦.
-    saveToDos();
+    saveToDos(event);
 }
 
 
-toDoForm.addEventListener("submit",handleToDoSubmit);
+toDoForm[0].addEventListener("submit",handleToDoSubmit);
+toDoForm[1].addEventListener("submit",handleToDoSubmit);
+toDoForm[2].addEventListener("submit",handleToDoSubmit);
 
-const savedToDos = localStorage.getItem(TODOS_KEY);
-if(savedToDos){
-    const parsedToDos = JSON.parse(savedToDos);
-    toDos = parsedToDos;
-    parsedToDos.forEach(paintToDo); //array 각 요소마다 forEach안에 있는 함수를 적용하여 실행, 즉 요소 개수만큼 실행된다.
-}
+
+// for(let i = 0 ; i < toDoFormSize ; i++)
+// {
+//     const savedToDos = localStorage.getItem(TODOS_KEY[i]);
+//     if(savedToDos){
+//         const parsedToDos = JSON.parse(savedToDos);
+//         let toDo = findToDos(i);
+//         toDo = parsedToDos;
+//         for(let k = 0 ; k < parsedToDos.length ; k++){
+
+//         }
+//         // parsedToDos.forEach(paintToDo); //array 각 요소마다 forEach안에 있는 함수를 적용하여 실행, 즉 요소 개수만큼 실행된다.
+//     }
+// }
