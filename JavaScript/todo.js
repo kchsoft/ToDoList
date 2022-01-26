e39bc30ccce3953eb5464695bb9223bb4441d1d2
changhyeon3 <53136613+changhyeon3@users.noreply.github.com>
2022-01-26 14:53:23 +0900

    TodoList 작성 부분 3개 구현 완료
    1. 웹페이지를 처음 열어 localStorage에서 값을 불러오는 경우, 단순히 findToDos() 함수를 사용하여 toDos1,2,3 의 배열을 반환하면, 메모리 주소만 넘기는 것이기 때문에, 효과가 없음. 따라서 toDos1,2,3 자체에 값을 넣어야 하기 때문에 setToDos() 함수를 새로 추가함.
    
    2. deleteToDo(), 삭제의 경우 또한, 1번과 같은 이유로 filterToDo() 함수를 추가하여 ToDoList 하나가 삭제되면 toDos1,2,3에 직접 새로운 배열을 만들 수 있도록 하였음.
    
    3. 이외에 잡다한 버그 수정함.

--- JavaScript/todo.js
+++ JavaScript/todo.js
@@ -11,48 +11,68 @@ const toDoFormSize= 3;
 let toDosnum;
 const TODOS_KEY=["form_1","form_2","form_3"];
 
+
+/*-----------------------------------------------------------------------------*/
+function setToDos(toDos,parsedToDos){
+    if(toDos === toDos1) toDos1 = parsedToDos;
+    if(toDos === toDos2) toDos2 = parsedToDos;
+    if(toDos === toDos3) toDos3 = parsedToDos;
+}
+
+function filterToDo(toDos,p){
+    if(toDos === toDos1) toDos1 = toDos1.filter((toDo) => toDo.id !== parseInt(p.id));
+    if(toDos === toDos2) toDos2 = toDos2.filter((toDo) => toDo.id !== parseInt(p.id));
+    if(toDos === toDos3) toDos3 = toDos3.filter((toDo) => toDo.id !== parseInt(p.id));
+    // filter -> array에 요소 하나씩 꺼내어 ( )안에 각각 적용한다.
+    // toDo로 요소하나를 받고, => 오른쪽으로 연산을 하며 true면 새로운 array에 포함,
+    // false면 새로운 array에서 제외한다.
+    // 결과적으로 true가 반환되는 toDo를 새로운 array에 포함한다. -> 새로운 array 만듦.
+}
+
 function findToDos(event){
-    if(event.target === toDoForm[0]) return toDos1;
-    if(event.target === toDoForm[1]) return toDos2;
-    if(event.target === toDoForm[2]) return toDos3;
+    if(event.target === toDoForm[0] || event == toDoForm[0]) return toDos1;
+    if(event.target === toDoForm[1] || event == toDoForm[1]) return toDos2;
+    if(event.target === toDoForm[2] || event == toDoForm[2]) return toDos3;
 
     if(event === 0) return toDos1;
     if(event === 1) return toDos2;
     if(event === 2) return toDos3;
 }
 
-function findToDoForm(event){
-    if(event.target === toDoForm[0]) return event.target;
-    if(event.target === toDoForm[1]) return event.target;
-    if(event.target === toDoForm[2]) return event.target;
+function findToDoFormInput(event){
+    if(event.target === toDoForm[0]) return toDoInput[0];
+    if(event.target === toDoForm[1]) return toDoInput[1];
+    if(event.target === toDoForm[2]) return toDoInput[2];
 }
 
 function findToDoFormNumber(event){  
-    if(event.target === toDoForm[0]) return 0;
-    if(event.target === toDoForm[1]) return 1;
-    if(event.target === toDoForm[2]) return 2;
+    if(event.target === toDoForm[0] || event == toDoForm[0]) return 0;
+    if(event.target === toDoForm[1] || event == toDoForm[1]) return 1;
+    if(event.target === toDoForm[2] || event == toDoForm[2]) return 2;
+
+    if(event === 0) return 0;
+    if(event === 1) return 1;
+    if(event === 2) return 2;
 
 }
 
 function handleToDoSubmit(event){
     event.preventDefault();
-    const toDoInputTarget = findToDoForm(event);
+    const toDoInputTarget = findToDoFormInput(event);
     const newTodo = toDoInputTarget.value;
     toDoInputTarget.value="";
     const newTodoObj = {
         text : newTodo,
         id : Date.now()
     };
-
-    const toDo = findToDos(event);
-    toDo.push(newTodoObj);
+    
+    const toDos = findToDos(event);
+    toDos.push(newTodoObj);
     paintToDo(newTodoObj,event);
     saveToDos(event);
 }
 
 function paintToDo(newTodo,event){
-    // if(newTodo === null) return;
-
     const p = document.createElement("p");
     p.id = newTodo.id;
     
@@ -75,31 +95,31 @@ function saveToDos(event){
 function deleteToDo(event){
     const p = event.target.parentElement; // event.target["parentElement"]도 가능
     p.remove();
-
-    toDos = toDos.filter((toDo) => toDo.id !== parseInt(p.id));
-    // filter -> array에 요소 하나씩 꺼내어 ( )안에 각각 적용한다.
-    // toDo로 요소하나를 받고, => 오른쪽으로 연산을 하며 true면 새로운 array에 포함,
-    // false면 새로운 array에서 제외한다.
-    // 결과적으로 true가 반환되는 toDo를 새로운 array에 포함한다. -> 새로운 array 만듦.
-    saveToDos(event);
+    
+    const foundToDoForm = event.path[3].firstElementChild;
+    let toDos = findToDos(foundToDoForm);
+    filterToDo(toDos,p);
+    
+    saveToDos(foundToDoForm);
 }
 
 
+/*-------------------------------------------------------*/ 
 toDoForm[0].addEventListener("submit",handleToDoSubmit);
 toDoForm[1].addEventListener("submit",handleToDoSubmit);
 toDoForm[2].addEventListener("submit",handleToDoSubmit);
 
+for(let FormNumber = 0 ; FormNumber < toDoFormSize ; FormNumber++)
+{
+    const savedToDos = localStorage.getItem(TODOS_KEY[FormNumber]);
+    if(savedToDos){
+        const parsedToDos = JSON.parse(savedToDos);
+        setToDos(findToDos(FormNumber),parsedToDos);
+        for(let ListNumber = 0 ; ListNumber < parsedToDos.length; ListNumber++){
+            paintToDo(parsedToDos[ListNumber],FormNumber);
+        }
+        // parsedToDos.forEach(paintToDo); //array 각 요소마다 forEach안에 있는 함수를 적용하여 실행, 즉 요소 개수만큼 실행된다.
+    }
+}
 
-// for(let i = 0 ; i < toDoFormSize ; i++)
-// {
-//     const savedToDos = localStorage.getItem(TODOS_KEY[i]);
-//     if(savedToDos){
-//         const parsedToDos = JSON.parse(savedToDos);
-//         let toDo = findToDos(i);
-//         toDo = parsedToDos;
-//         for(let k = 0 ; k < parsedToDos.length ; k++){
 
-//         }
-//         // parsedToDos.forEach(paintToDo); //array 각 요소마다 forEach안에 있는 함수를 적용하여 실행, 즉 요소 개수만큼 실행된다.
-//     }
-// }
