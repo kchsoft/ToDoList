85b0c3a6c7528e66b1df32000f9d056b40b84314
changhyeon3 <53136613+changhyeon3@users.noreply.github.com>
2022-01-28 20:57:02 +0900

    ToDoList의 Title 입력 구현 중
    1. Title을 입력받아 DB에 저장하는 것 구현
    
    2. Title을 입력받으면 form에 hidden 클래스가 추가되어 form이 사라지고, h3에 hidden 클래스가 삭제되어 h3(Title)만 남겨진다.
    +44 , +47 index.html

--- JavaScript/todo_title.js
+++ JavaScript/todo_title.js
@@ -1,15 +1,35 @@
-const toDoTitle = document.querySelectorAll(".todo-title");
-const toDoTitleSize = toDoTitle.length;
+const toDoTitleForm = document.querySelectorAll(".todo-title");
+const toDoTitle = document.querySelectorAll(".title");
+const toDoTitleSize = toDoTitleForm.length;
+const TITLE_KEY = ["title_1","title_2","title_3"];
+
 
 /*----------------------------------*/
+function findTitleKeyIndex(event){
+    if(event.target === toDoTitleForm[0]) return 0;
+    if(event.target === toDoTitleForm[1]) return 1;
+    if(event.target === toDoTitleForm[2]) return 2;
+}
 
 function handleToDoTitle(event){
     event.preventDefault();
+    const toDoInput = event.target.firstElementChild;
+    const Title = toDoInput.value
+    toDoInput.value = "";
+    const TitleKeyIndex = findTitleKeyIndex(event);
+    toDoInput.classList.add(HIDDEN_CLASSNAME);
+
+    localStorage.setItem(TITLE_KEY[TitleKeyIndex],Title);
+    paintTitle(Title,TitleKeyIndex);
 }
 
+function paintTitle(Title,Index){
+    toDoTitle[Index].innerHTML = `${Title}`;
+    toDoTitle[Index].classList.remove(HIDDEN_CLASSNAME);
+}
 /*----------------------------------*/
 
 
 for(let i = 0; i < toDoTitleSize; i ++){
-    toDoTitle[i].addEventListener("submit",handleToDoTitle);
+    toDoTitleForm[i].addEventListener("submit",handleToDoTitle);
 }
